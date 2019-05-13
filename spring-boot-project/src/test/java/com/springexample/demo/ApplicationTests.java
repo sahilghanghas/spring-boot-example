1c04ae0d6b1bb1f84b64faccb3a8ec8955d136e3
sahil <sahilghanghas93@gmail.com>
2019-05-13 17:14:07 -0400

    Update ApplicationTests.java

--- spring-boot-project/src/test/java/com/springexample/demo/ApplicationTests.java
+++ spring-boot-project/src/test/java/com/springexample/demo/ApplicationTests.java
@@ -1,7 +1,12 @@
 package com.springexample.demo;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import com.springexample.model.UserRepository;
+import com.springexample.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTests {
 
+    @Autowired
+    UserRepository userRepository;
     @Test
     public void contextLoads() {
+        User user = new User(1,"name");
+        userRepository.save(user);
+        User user1 = userRepository.findUserById(user.getId());
+        assertNotNull(user1);
+        assertEquals(user.getName(),user1.getName());
     }
 
 }
